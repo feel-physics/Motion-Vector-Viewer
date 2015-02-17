c3d8f6803aac9116ba65acb13c8478575ec41f36
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2015-02-17 12:11:12 +0900

    🚧カーブフィルタの準備である補完関数を実装した

--- codes/opencv-python-book/3rd-typing/cameo/filters.py
+++ codes/opencv-python-book/3rd-typing/cameo/filters.py
@@ -5,23 +5,72 @@ import cv2
 import numpy
 import utils
 
-class BGRFuncFilter(object):
+def recolorRC(src, dst):
     """
-
+    BGRからRC（赤、シアン）への変換をシミュレーションする
+    コードの内容：
+    dst.b = dst.g = 0.5 * (src.b + src.g)
+    dst.r = src.r
+    :param src: BGR形式の入力画像
+    :param dst: BGR形式の出力画像
+    :return: None
     """
+    b, g, r = cv2.split(src)
+    cv2.addWeighted(b, 0.5, g, 0.5, 0, b)
+    # Python: cv.AddWeighted(src1, alpha, src2, beta, gamma, dst) → None
+    # Parameters:
+    # src1 – first input array.
+    # alpha – weight of the first array elements.
+    # src2 – second input array of the same size and channel number as src1.
+    # beta – weight of the second array elements.
+    # dst – output array that has the same size and number of channels as the input arrays.
+    # gamma – scalar added to each sum.
+    # dtype – optional depth of the output array; when both input arrays have the same depth, dtype can be set to -1, which will be equivalent to src1.depth().
+    # The function addWeighted calculates the weighted sum of two arrays as follows:
+    # dst = src1 * alpha + src2 * beta + gamma
+    cv2.merge((b, b, r), dst)
 
-    def __init__(self, vFunc = None, bFunc = None, gFunc = None,
-                 rFunc = None, dtype = numpy.unit8):
-        lengh = numpy.iinfo(dtype).max + 1
-        self._vLookupArray = utils.createLookupArray(vFunc, lengh)
+def recolorRGV(src, dst):
+    """
+    BGRからRGV（赤、緑、値）への変換をシミュレートする
+    コードの内容：
+    dst.b = min(src.b, src.g, src.r)
+    dst.g = src.g
+    dst.r = src.r
+    :param src: BGR形式の入力画像
+    :param dst: BGR形式の出力画像
+    :return: None
+    """
+    b, g, r = cv2.split(src)
+    cv2.min(b, g, b)
+    cv2.min(b, r, b)
+    # Python: cv2.min(src1, src2[, dst]) → dst
+    # Python: cv.Min(src1, src2, dst) → None
+    # Python: cv.MinS(src, value, dst) → None
+    # Parameters:
+    # src1 – first input array.
+    # src2 – second input array of the same size and type as src1.
+    # value – real scalar value.
+    # dst – output array of the same size and type as src1.
+    # The functions min calculate the per-element minimum of two arrays:
+    #     dst = min(src1, src2)
+    # or array and a scalar:
+    #     dst = min(src1, value)
+    # In the second variant, when the input array is multi-channel, each channel is compared with value independently.
+    cv2.merge((b, g, r), dst)
 
-    def apply(self, src, dst):
-        """
-
-        :param src:
-        :param dst:
-        :return:
-        """
-
-
-class BGRPortalCurveFilter
+def recolorCMV(src, dst):
+    """
+    BGRからCMV（シアン、マゼンタ、値）への変換をシミュレートする
+    コードの内容：
+    dst.b = max(src.b, src.g, src.r)
+    dst.g = src.g
+    dst.r = src.r
+    :param src: BGR形式の入力画像
+    :param dst: BGR形式の出力画像
+    :return: None
+    """
+    b, g, r = cv2.split(src)
+    cv2.max(b, g, b)
+    cv2.max(b, r, b)
+    cv2.merge((b, g, r), dst)
--- codes/opencv-python-book/3rd-typing/cameo/utils.py
+++ codes/opencv-python-book/3rd-typing/cameo/utils.py
@@ -77,3 +77,33 @@ def createLookupArray(func, length = 256):
         i += 1
     return lookupArray
 
+def createCurveFunc(points):
+    """
+    制御点を元にした関数を返す
+    xが入力、yが出力なので制御点(128, 160)はより明るくする
+    :param points: 制御点
+    :return:
+    """
+    if points is None:
+        return None
+    numPoints = len(points)
+    if numPoints < 2:
+        return None
+    xs, ys = zip(*points)
+    # Works like itertools.izip().
+    #
+    ### TODO: これは何をやっているんだ？
+    ### TODO: 仮説(x1,y1),(x2,y2)->(x1,x2),(y1,y2)
+    #
+    if numPoints < 4:
+        kind = 'linear'
+        # 'quadratic'（ベジェ曲線のようなもの） is not implemented
+    else:
+        kind = 'cubic' # ベジェ曲線
+    return scipy.interpolate.interp1d(xs, ys, kind,
+                                      bounds_error=False)
+    # class scipy.interpolate.interp1d(x, y, kind='linear')
+    # Interpolate a 1-D function.
+    #
+    # x and y are arrays of values used to approximate some function f: y = f(x).
+    # This class returns a function whose call method uses interpolation to find the value of new points.
