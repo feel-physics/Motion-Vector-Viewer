40a0c11f734272df17332a24dd3f97404011d21f
Tatsuro Ueda <ueda.tatsuro@gmail.com>
2015-07-23 16:46:23 +0900

    🚀MoGの引数を変えやすくした

--- 150721-背景除去/mog.py
+++ 150721-背景除去/mog.py
@@ -2,18 +2,25 @@
 __author__ = 'weed'
 
 import cv2
-import lib_video
 
 cap = cv2.VideoCapture(0)
 
 
-videoWriter = lib_video.initVideoRecoder(cap)  # Video Record Code
+# Video Record Code
+import lib_video
+videoWriter = lib_video.initVideoRecoder(cap)
 
 
-fgbg = cv2.BackgroundSubtractorMOG()
+fgbg = cv2.BackgroundSubtractorMOG(
+        200  # default
+        # 1000
+    , 5, 0.7, 0)
 while True:
     ret, src = cap.read()
-    fgmask = fgbg.apply(src, learningRate=0.01)
+    fgmask = fgbg.apply(src, learningRate=
+        0  # default
+        # 0.001
+    )
     dst = src.copy()
     dst = cv2.bitwise_and(src, src, mask=fgmask)
     cv2.imshow('frame',dst)
